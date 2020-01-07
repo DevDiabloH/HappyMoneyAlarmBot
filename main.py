5e4354ad355258208c7b3ca9bd6174a58ece7456
DevDiabloH <50089624+DevDiabloH@users.noreply.github.com>
2020-01-07 14:30:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,50 +4,62 @@ import urllib
 import winsound
 import threading
 import webbrowser
-import os
 import telegram
-
+import os
 
 class AsyncTask:
     def __init__(self):
         self.my_token = []
         self.bot = []
-        self.data = []
         self.targetUrl = []
         self.url = []
         pass
+
+    def SetupFile(self):
+        f = open('prevLink.txt', 'a')
+        f.close()
 
     def SetupBrowser(self):
         self.targetUrl = "http://www.ppomppu.co.kr/zboard/"
         self.url = "http://www.ppomppu.co.kr/zboard/zboard.php?id=ppomppu"
 
     def SetupTelegram(self):
-        self.my_token = 'YourToken'
+        self.my_token = 'Token'
         self.bot = telegram.Bot(token=self.my_token)
         threading.Timer(60, self.SetupTelegram).start()
 
     def FindHappy(self):
-        findStr = "해피머니"
+        str = "해피머니"
         html = urlopen(self.url)
         bsObject = BeautifulSoup(html, "html.parser")
 
         for link in bsObject.find_all('a'):
-            if link.text.find(findStr) != -1:
+            if link.text.find(str) != -1:
                 if link.text.find("[") != -1:
                     os.system('cls')
-                    if self.data != self.targetUrl + link.get('href'):
-                        print(link.text.strip(), targetUrl + link.get('href'))
-                        self.data = self.targetUrl + link.get('href')
-                        webbrowser.open(self.targetUrl + link.get('href'))
-                        for i in range(0, 9):
-                            self.bot.sendMessage(chat_id='@HappyObserver', text=(self.targetUrl + link.get('href')))
+                    urlData = self.targetUrl + link.get('href')
+                    read = open('prevLink.txt', mode='r', encoding='utf-8')
+
+                    findLink = link.get('href').split('&no=')[1];
+                    prevLink = read.readline();
+                    read.close()
+
+                    if(findLink != prevLink):
+                        f = open('prevLink.txt', mode='wt', encoding='utf-8')
+                        f.write(findLink)
+                        f.close()
+                        webbrowser.open(urlData)
+                        for i in range(0, 5):
+                            self.bot.sendMessage(chat_id='@HappyObserver',
+                                                 text=(link.text.strip(), urlData))
                     break
 
         print("Try Finding")
-        threading.Timer(20, self.FindHappy).start()
+        threading.Timer(5, self.FindHappy).start()
 
 
 at = AsyncTask()
+at.SetupFile()
 at.SetupBrowser()
 at.SetupTelegram()
 at.FindHappy()
